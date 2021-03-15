968314df8776e706cf9b91449ef4ade3510236c3
czqu <ryan.h.qin@outlook.com>
2021-03-15 18:13:26 +0800

    init

---
+++ src/main/java/cn/team2084/syn/SynApplication.java
@@ -0,0 +1,13 @@
+package cn.team2084.syn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SynApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/team2084/syn/SynApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.team2084.syn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
